b462fb0a3fbf0f5157f5213b698b4a764939356f
YeonjaeMan <jgg9809@naver.com>
2024-11-21 14:14:40 +0900

    first commit

---
+++ src/main/java/com/ktr/glp_pro/GlpProApplication.java
@@ -0,0 +1,13 @@
+package com.ktr.glp_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlpProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlpProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktr/glp_pro/GlpProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktr.glp_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlpProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
